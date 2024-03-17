a429f604907a84117967eef3f2fd1b80d0ce4e54
Julio César de Souza <134759649+Dom-Furia@users.noreply.github.com>
2024-03-17 18:23:32 -0400

    Add files via upload

--- Projeto_final/Projeto_CyberEedux/urls.py
+++ Projeto_final/Projeto_CyberEedux/urls.py
@@ -11,4 +11,7 @@ urlpatterns = [
     path('adicionar/', views.adicionar_aluno, name='adicionar_aluno'),
     path('matricular_aluno/', views.matricular_aluno, name='matricular_aluno'),
     path('criar_curso/', views.criar_curso, name='criar_curso'),
+    path('cadastrar_professor/',views.cadastrar_Professor, name='cadastrar_professor'),
+    path('cadastrar_departamento/',views.Cadastrar_departamento, name='cadastrar_departamento'),
+
 ]
--- Projeto_final/sistema_academico/views.py
+++ Projeto_final/sistema_academico/views.py
@@ -20,22 +20,22 @@ def delete_aluno(request, id):
 def editar_aluno(request, id):
     alunos = Aluno.objects.get(id=id)
     if request.method == "POST":
-        alunos.nome = request.POST['nome']
-        alunos.matricula = request.POST['matricula']
-        alunos.telefone = request.POST['telefone']
+        alunos.nome = request.POST.get('nome')
+        alunos.matricula = request.POST.get('matricula')
+        alunos.telefone = request.POST.get('telefone')
         alunos.save()
         return redirect('home')
     return render(request, 'editar_aluno.html', {'alunos': alunos})
 
 def adicionar_aluno(request):
     if request.method == "POST":
-        nome = request.POST['nome']
-        matricula = request.POST['matricula']
-        telefone = request.POST['telefone']
-        cpf = request.POST['cpf']
-        endereco = request.POST['endereco']
-        email = request.POST['email']
-        data_nascimento = request.POST['data_nascimento']
+        nome = request.POST.get('nome')
+        matricula = request.POST.get('matricula')
+        telefone = request.POST.get('telefone')
+        cpf = request.POST.get('cpf')
+        endereco = request.POST.get('endereco')
+        email = request.POST.get('email')
+        data_nascimento = request.POST.get('data_nascimento')
         Aluno.objects.create(nome=nome, matricula=matricula, telefone=telefone,cpf=cpf,endereco=endereco,data_nascimento=data_nascimento,email=email)
         return redirect('home')
     return render(request, 'adicionar_aluno.html')
@@ -51,6 +51,7 @@ def criar_curso(request):
         else:
             try:
                 cursos.create(nome=nome, descricao=descricao,duracao=50,coordenador='Felipe',carga_horaria=120)
+                cursos.save()
                 messages.success(request, 'Curso criado com sucesso!')
                 return redirect('criar_curso')
             except Exception as e:
@@ -68,11 +69,13 @@ def listar_cursos_e_departamentos(request):
 
 def matricular_aluno(request):
     if request.method == 'POST':
-        aluno_id = request.POST['aluno']
-        curso_id = request.POST['curso']
+        aluno_id = request.POST.get('aluno')
+        curso_id = request.POST.get('curso')
+        print(aluno_id)
         
         aluno = Aluno.objects.get(id=aluno_id)
         curso = Curso.objects.get(id=curso_id)
+        print(aluno)
 
         if  Matricula.objects.filter(aluno=aluno, curso=curso).exists():
             messages.error(request, 'Este aluno (a) ja esta matriculado (a) neste curso.')
@@ -81,6 +84,7 @@ def matricular_aluno(request):
             try:
                 Matricula.objects.create(aluno=aluno, curso=curso)
                 messages.success(request, 'Aluno (a) matriculado com sucesso!')
+                Matricula.objects.save()
                 return redirect('matricular_aluno')
             except Exception as e:
                 messages.error(request, f'Erro ao matricular aluno (a): {e}')
@@ -88,6 +92,50 @@ def matricular_aluno(request):
     else:
         alunos = Aluno.objects.all()
         cursos = Curso.objects.all()
-        
         return render(request, 'matricular_aluno.html', {'alunos': alunos, 'cursos': cursos})
+
+def cadastrar_Professor(request):
+    professor = Professor.objects.all()
+    departamentos = Departamento.objects.all()
+    if request.method == "POST":
+        nome = request.POST.get('nome')
+        disciplinas = request.POST.get('disciplinas')
+        telefone = request.POST.get('telefone')
+        departamento_id = request.POST.get('departamento')
+        dep = Departamento.objects.get(id=departamento_id)
+        print(departamento_id)
+        if professor.filter(nome=nome).exists():
+            messages.error(request, 'Já existe um professor com este nome.')
+            return redirect('cadastrar_professor')
+        else:
+            try:
+                professor.create(nome=nome,email = 'curso@cyberedux.com.br',telefone = telefone, data_contratacao='2024-1-15', salario = 4000.78, disciplina_ministrada = disciplinas,endereco = 'Rua B', departamento = dep)
+                messages.success(request, 'Professor adicionado com sucesso!')
+                return redirect('cadastrar_professor')
+            except Exception as e:
+                messages.error(request, f'Erro ao criar professor: {e}')
+                return redirect('cadastrar_professor')     
+    return render(request, 'cadastrar_professor.html', {'professor':professor, 'departamentos':departamentos})
+
+def Cadastrar_departamento(request):
+    departamentos = Departamento.objects.all()
+    if request.method == "POST":
+        nome = request.POST['nome']
+        descricao = request.POST['descricao']
+        telefone = request.POST['telefone']
+        email = request.POST['email']
+        if departamentos.filter(nome=nome).exists():
+            messages.error(request, 'Já existe um departamento com este nome.')
+            return redirect('cadastrar_departamento')
+        else:
+            try:
+                departamentos.create(nome=nome, descricao=descricao,telefone=telefone,email=email)
+                messages.success(request, 'Curso criado com sucesso!')
+                return redirect('cadastrar_departamento')
+            except Exception as e:
+                messages.error(request, f'Erro ao criar curso: {e}')
+                return redirect('cadastrar_departamento')
+            
+    return render(request, 'cadastrar_departamento.html', {'departamentos':departamentos})
+
 
